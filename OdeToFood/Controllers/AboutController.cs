696c06c98984dc9f5d413cb716201dabd5638120
shekharr <shekharr@tavisca.com>
2018-12-18 18:09:41 +0530

    initial

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "+91 8884004488";
+        }
+
+        public string Address()
+        {
+            return "Viman Nagar ,Pune";
+        }
+    }
+}
